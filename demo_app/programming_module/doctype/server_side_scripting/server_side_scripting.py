025c149902537785d6e6e7226ea956546736972d
Atiq0629 <cross.016x@gmail.com>
2022-12-01 00:54:42 +0600

    Server side scripting - get_doc, new_doc, delete_doc

--- demo_app/programming_module/doctype/server_side_scripting/server_side_scripting.py
+++ demo_app/programming_module/doctype/server_side_scripting/server_side_scripting.py
@@ -57,17 +57,61 @@ class ServerSideScripting(Document):
 
 
 	#Fetch Value from parent
-	def validate(self):
-		frappe.msgprint(_("Hello, My Full Name is '{0}' ").format(
-			self.first_name +" "+self.middle_name +" "+self.last_name
-		))
+	# def validate(self):
+	# 	frappe.msgprint(_("Hello, My Full Name is '{0}' ").format(
+	# 		self.first_name +" "+self.middle_name +" "+self.last_name
+	# 	))
 
 
 	#Fetch Value from child
 
+	# def validate(self):
+	# 	for row in self.get("family_members"):
+	# 		frappe.msgprint(_(
+	# 			"{0}. The family_member is '{1}' and relation is '{2}'").format(
+	# 				row.idx, row.name1, row.relation 
+	# 		))
+
+
+
+	#get_doc
+
+	# def validate(self):
+	# 	self.get_document()
+	
+	# def get_document(self):
+	# 	doc = frappe.get_doc("Client Side Scripting", self.client_side_doc)
+	# 	frappe.msgprint(_("The First Name is {0}, Last Name is {1} and Age is {2}").format(doc.first_name, doc.last_name, doc.age))
+		
+	# 	#From child Tbale
+	# 	for row in doc.get("family_members"):
+	# 		frappe.msgprint(_("{0}. The Family member name is {1} and relation is {2}").format(row.idx, row.name1, row.relation))
+	 
+
+
+
+	# new_doc()
+
 	def validate(self):
-		for row in self.get("family_members"):
-			frappe.msgprint(_(
-				"{0}. The family_member is '{1}' and relation is '{2}'").format(
-					row.idx, row.name1, row.relation 
-			))
+		self.new_document()
+
+	def new_document(self):
+		doc = frappe.new_doc("Client Side Scripting")
+		doc.first_name = "Jake2"
+		doc.last_name = "Sully2"
+		doc.age = 302
+		#child Table
+		doc.append("family_members", 
+		{
+			"name1" : "Sagar",
+			"relation": "Vara",
+			"age": 25
+		})
+
+		doc.insert()
+
+
+	# delete_doc
+	def validate(self):
+		frappe.delete_doc("Client Side Scripting", "PR-0007")
+
